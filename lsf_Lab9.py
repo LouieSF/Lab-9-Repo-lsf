c5403b395fa44ab5f579d38cce74dfe380f22bf4
LouieSF <lsf2004impo@gmail.com>
2024-07-26 21:56:16 -0400

    added main menu function

--- lsf_Lab9.py
+++ lsf_Lab9.py
@@ -1,4 +1,3 @@
-
 def encode(password):
     encoded = ""
     for i in password:
@@ -14,9 +13,37 @@ def encode(password):
         encoded += str(num)
     return encoded
 
+
+def decode(password):
     decode = ""
     for i in password:
         if '0' <= i <= '9':
             decoded_i = str((int(i) - 3) % 10)
             decode += decoded_i
     return decode
+
+
+def main():
+    remain = True
+    while remain:
+        print("Menu\n"
+              "-------------\n"
+              "1. Encode\n"
+              "2. Decode\n"
+              "3. Quit\n")
+        choice = int(input("Please enter an option: "))
+        if choice == 1:
+            password = input("Please enter your password to encode: ")
+            encoded = encode(password)
+            print("Your password has been encoded and stored!\n")
+        elif choice == 2:
+            decoded = decode(encoded)
+            print("The encoded password is ", encoded, " and the original password is ", decoded, ".\n", sep="")
+        elif choice == 3:
+            remain = False
+        else:
+            print("Invalid input.\n")
+
+
+if __name__ == "__main__":
+    main()
